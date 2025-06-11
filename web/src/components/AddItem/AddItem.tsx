e2001d9d9e591eefafaf353f056f63f157e30579
Diego Suarez <suarez.pino.diego@gmail.com>
2025-06-10 22:08:46 -0400

    feat empty additem

---
+++ web/src/components/AddItem/AddItem.tsx
@@ -0,0 +1,10 @@
+export const AddItem: React.FC = () => {
+    return (
+        <div className="add-item">
+            <h2>Add New Item</h2>
+            <form>
+
+            </form>
+        </div>
+    );
+}
